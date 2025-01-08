a209e3952f236670c53a4953bb241ffcb5dbf546
minh <enoeul.03@gmail.com>
2025-01-08 20:07:51 +0700

    under the hood, không được thì cook

--- src/_27_Remove_Element.java
+++ src/_27_Remove_Element.java
@@ -1,23 +1,15 @@
 public class _27_Remove_Element {
     public static int removeElement(int[] nums, int val) {
-        int soPhanTu = nums.length;
-        for(int i = 0;i < soPhanTu;){
-            if(nums[i] == val){
-                xoaPhanTu(i,nums,soPhanTu);
-                soPhanTu--;
-                continue;
+        int k = 0;
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
             }
-            i++;
         }
-        return soPhanTu;
+        return k;
     }
 
-    public static void xoaPhanTu(int i, int[] nums, int soPhanTu) {
-        for(int j = i;j < soPhanTu-2;j++){
-            nums[j] = nums[j+1];
-        }
-        nums[soPhanTu-1] = 0;
-    }
 
     public static void main(String[] args) {
         int[] nums = {1,2,3,3,4,3,4,5};
--- src/_88_Merge_Sorted_Array.java
+++ src/_88_Merge_Sorted_Array.java
@@ -2,29 +2,24 @@ import java.util.Arrays;
 
 public class _88_Merge_Sorted_Array {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int a : nums2){
-            chenPhanTuVaoMang(a, nums1, m);
-            m++;
-        }
-    }
-
-    public static void chenPhanTuVaoMang(int a, int[] nums1, int m) {
-        boolean daThay = false;
-        for(int i = 0;i < m;i++){
-            if (a < nums1[i]){
-                daThay = true;
-                for(int j = m-1;j >= i;j--){
-                    nums1[j+1] = nums1[j];
-                }
-                nums1[i] = a;
-                break;
+        for(int k = m+n-1;k >= 0;k--){
+            if(n<=0){
+                nums1[k] = nums1[m-1];
+                m--;
+            }else if (m<=0){
+                nums1[k] = nums2[n-1];
+                n--;
+            }
+            else if(nums1[m-1]>nums2[n-1]){
+                nums1[k] = nums1[m-1];
+                m--;
+            }else{
+                nums1[k] = nums2[n-1];
+                n--;
             }
         }
-
-        if(!daThay){
-            nums1[m] = a;
-        }
     }
+
 
     public static void main(String[] args) {
         int[] n1 = {1,2,3,0,0,0};
