abfeba2fa085150a1a8f84ee617ea8df911767df
minh <enoeul.03@gmail.com>
2025-01-09 16:09:15 +0700

    under the hood, không được thì cook

--- src/_941_Valid_Mountain_Array.java
+++ src/_941_Valid_Mountain_Array.java
@@ -1,26 +1,24 @@
 public class _941_Valid_Mountain_Array {
     public static boolean validMountainArray(int[] arr) {
         if(arr.length <= 2) return false;
-        int dinh = 0;
+        int leftPeak = 0;
+        int rightPeak = arr.length - 1;
         for(int i = 0; i < arr.length-1; i++){
             if(arr[i] >= arr[i+1]){
-                if(arr[i] == arr[i+1]){
-                    return false;
-                }
-                dinh = i;
+                leftPeak = i;
                 break;
             }
         }
-        for(int i = dinh; i < arr.length-1; i++){
-            if(arr[i+1] >= arr[i]){
-                return false;
+        for(int i = arr.length - 1; i > 0; i--){
+            if(arr[i-1] <= arr[i]){
+                rightPeak = i;
+                break;
             }
         }
-        if (dinh == 0 || dinh ==arr.length)   return false;
-        return true;
+        return leftPeak == rightPeak;
     }
     public static void main(String[] args) {
-        int[] arr = {14,82,89,84,79,70,70,68,67,66,63,60,58,54,44,43,32,28,26,25,22,15,13,12,10,8,7,5,4,3};
+        int[] arr = {1,2,3,4};
         System.out.println(validMountainArray(arr));
     }
 }
