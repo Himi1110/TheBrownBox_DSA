ef95a1d85959b15ef931bfdeddbc3a2a6f80b09c
minh <enoeul.03@gmail.com>
2025-01-09 19:07:48 +0700

    under the hood, không được thì cook

--- src/_1221_Split_a_String_in_Balanced_Strings.java
+++ src/_1221_Split_a_String_in_Balanced_Strings.java
@@ -1,24 +1,24 @@
 public class _1221_Split_a_String_in_Balanced_Strings {
     public static int balancedStringSplit(String s) {
         int count = 0;
-        int[] nums = new int[2];
+        int balance = 0;
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i)=='L'){
-                nums[0]++;
-                if(nums[0]==nums[1]){
-                    count++;
-                }
+                balance++;
             }else{
-                nums[1]++;
-                if(nums[0]==nums[1]){
-                    count++;
-                }
+                balance--;
+            }
+            if(balance==0){
+                count++;
             }
         }
+//        if(balance!=0){
+//            return 0;
+//        }
         return count;
     }
     public static void main(String[] args) {
-        String minh = "LRLRLRRRLL";
+        String minh = "RLLLL";
         System.out.println(balancedStringSplit(minh));
     }
 }
