3e2e7b93c5cf978c8836121e2c9c12b81c0d5bc5
minh <enoeul.03@gmail.com>
2025-01-09 15:43:18 +0700

    under the hood, không được thì cook

--- src/_941_Valid_Mountain_Array.java
+++ src/_941_Valid_Mountain_Array.java
@@ -1,28 +1,26 @@
 public class _941_Valid_Mountain_Array {
     public static boolean validMountainArray(int[] arr) {
+        if(arr.length <= 2) return false;
         int dinh = 0;
-        if(arr.length <= 2 || arr[0] >= arr[1]){
-            return false;
-        }
-        for (int i = 0; i < arr.length - 1; i++) {
-            if(arr[i]<arr[i+1]){
-                continue;
-            }else if(arr[i]==arr[i+1]){
-                return false;
-            }else{
+        for(int i = 0; i < arr.length-1; i++){
+            if(arr[i] >= arr[i+1]){
+                if(arr[i] == arr[i+1]){
+                    return false;
+                }
                 dinh = i;
                 break;
             }
         }
-        for(int i = dinh+1; i < arr.length; i++) {
-            if(arr[i] >=arr[i-1]){
+        for(int i = dinh; i < arr.length-1; i++){
+            if(arr[i+1] >= arr[i]){
                 return false;
             }
         }
+        if (dinh == 0 || dinh ==arr.length)   return false;
         return true;
     }
     public static void main(String[] args) {
-        int[] arr = {3,7,6,4,3,0,1,0};
+        int[] arr = {14,82,89,84,79,70,70,68,67,66,63,60,58,54,44,43,32,28,26,25,22,15,13,12,10,8,7,5,4,3};
         System.out.println(validMountainArray(arr));
     }
 }
